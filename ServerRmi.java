5f1687d986320e0a5b4637979953207dc6e71af0
Pinank08 <pinank96@gmail.com>
2020-02-08 13:18:30 +0530

    Create ServerRmi.java
    Server Rmi Java

---
+++ ServerRmi.java
@@ -0,0 +1,20 @@
+import java.rmi.Naming;
+public class ServerRmi
+{
+    calcserv()
+    {
+        try
+        {
+          calc c=new calcimpl();
+          Naming.rebind("rmi://localhost:1099/calcservice",c);
+        }
+        catch(Exception e)
+        {
+          System.out.println("Exception:"+e);
+        }
+        }
+    public static void main(String arg[])
+    {
+      new calcserv();
+    }
+}
