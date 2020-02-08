99eb297baf453efe5fde81fb2fc931b7924ac6b3
vidhiksharma <60808628+vidhiksharma@users.noreply.github.com>
2020-02-08 14:24:57 +0530

    Delete calc.java

--- calc.java
+++
@@ -1,9 +0,0 @@
-import java.rmi.Remote;
-import java.rmi.RemoteException;
-public interface calc extends Remote
-{
-public long addition(long a,long b)throws RemoteException;
-public long subtraction(long a,long b)throws RemoteException;
-public long multiplication(long a,long b)throws RemoteException;
-public long divition(long a,long b)throws RemoteException;
-}
