da8478febb774a1a0f085a199084fec4a2a2b3b1
Pinank08 <pinank96@gmail.com>
2020-02-08 13:44:59 +0530

    Revert "Add files via upload"
    This reverts commit 7e5e1adf23ccd0b63942851e0f32289b2bd262bf.

--- Calcu.java
+++
@@ -1,9 +0,0 @@
-import java.rmi.Remote;
-import java.rmi.RemoteException;
-	public interface Calcu extends Remote
-	{
-		public long add(long a,long b)throws RemoteException;
-		public long subtract(long a,long b)throws RemoteException;
-		public long multiply(long a,long b)throws RemoteException;
-		public long divide(long a,long b)throws RemoteException;
-	}
