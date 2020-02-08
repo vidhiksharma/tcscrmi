4734767086fe30ced975717d54580c87b7be6ff2
vidhiksharma <60808628+vidhiksharma@users.noreply.github.com>
2020-02-08 14:08:35 +0530

    Create implement.java

---
+++ implement.java
@@ -0,0 +1,25 @@
+import java.rmi.RemoteException;
+import java.rmi.server.UnicastRemoteObject;
+public class implement extends UnicastRemoteObject implements calc
+{
+protected implement() throws RemoteException
+{
+super();
+}
+public long addition(long a,long b)throws RemoteException
+{
+return a+b;
+}
+public long subtraction(long a,long b)throws RemoteException
+{
+return a-b;
+}
+public long multiplication(long a,long b)throws RemoteException
+{
+return a*b;
+}
+public long divition(long a,long b)throws RemoteException
+{
+return a/b;
+}
+}
